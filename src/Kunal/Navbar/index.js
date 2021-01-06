bbab7208f703ba0d8b794007be39f834156b1d74
Sahil Malek <sahil.malek@mail.utoronto.ca>
2021-01-05 22:19:24 -0500

    Changes

--- src/Kunal/Navbar/index.js
+++ src/Kunal/Navbar/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {FaBars} from 'react-icons/fa'
-import {Nav, 
-        NavbarContainer, 
-        NavLogo, 
-        MobileIcon, 
-        NavMenu, 
-        NavItem, 
-        NavLinks 
+import {Nav,
+        NavbarContainer,
+        NavLogo,
+        MobileIcon,
+        NavMenu,
+        NavItem,
+        NavLinks
     } from  './NavbarElements';
 
 export class Navbar extends Component {
@@ -16,25 +16,15 @@ export class Navbar extends Component {
         name:'',
         };
     }
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 110c5f8a35afeed0b707b15bed2f93a972fbae90
     componentDidMount() {
         const data = JSON.parse(localStorage.getItem('userData'));
-        let data1=data;  
+        let data1=data;
         console.log(data1.Name);
         this.setState({name: data1.Name})
-<<<<<<< HEAD
     };
 
     render(){
-=======
-      };
-
-      render(){
->>>>>>> 110c5f8a35afeed0b707b15bed2f93a972fbae90
         return (
             <Nav>
                 <NavbarContainer>
@@ -62,11 +52,11 @@ export class Navbar extends Component {
                             <NavLinks to='Sign Out'>Sign Out</NavLinks>
                         </NavItem>
                     </NavMenu>
-            
+
                 </NavbarContainer>
             </Nav>
         )
     }
 };
 
-export default Navbar
+export default Navbar;
