4eafbf9e3b99470d31a21a396f08d8bb8f100653
Sahil Malek <70777672+MxSahil@users.noreply.github.com>
2021-01-05 19:36:34 -0500

    Add files via upload

---
+++ src/components/Courses/CoursesElements.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const CoursesTilesCont = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-wrap: wrap;
+  flex-flow: row;
+  justify-content: space-around;
+`;
+export const CourseTile = styled.div`
+  background-color: #cccccc;
+  margin: 25px;
+  padding: 10px;
+  border: 3px solid #cccccc;
+  border-radius: 10px;
+`
