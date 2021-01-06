ddd5eef5a7c9b034c19aa670a84c92b05fccface
Sahil Malek <sahil.malek@mail.utoronto.ca>
2021-01-05 19:30:35 -0500

    Courses components added

---
+++ src/components/Courses/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {CourseTilesCont, CourseTile}
+
+const Courses = () => {
+  return (
+    // <CoursePage>
+      // <CoursePageTitle>
+      //
+      // </CoursePageTitle>
+
+      <CoursesTilesCont>
+        <CourseTile>Classes Today</CourseTile>
+        <CourseTile>Calendar</CourseTile>
+        <CourseTile>GPA Calculator</CourseTile>
+        <CourseTile>Grade Calculator</CourseTile>
+      </CoursesTilesCont>
+    // </CoursePage>
+
+  );
+
+};
+
+export default Courses;
