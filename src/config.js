ec13755c09bbc68af9379525aecdc666f8c1e0c0
Sahil Malek <>
2021-01-06 20:16:11 -0500

    Database added

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  db: {
+    username: "theboys",
+    connection: "mongodb+srv://theboys:mLJ275HqGF8LAn85@mybudget.nkllr.mongodb.net/MyBudget?retryWrites=true&w=majority",
+  }
+}
+module.exports = config;
