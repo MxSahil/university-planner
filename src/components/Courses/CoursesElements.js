8f51e7574719391acc7d7c15655422c4f9eee236
Sahil Malek <>
2021-01-06 19:24:35 -0500

    Updated Courses

--- src/components/Courses/CoursesElements.js
+++ src/components/Courses/CoursesElements.js
@@ -22,11 +22,8 @@ export const CourseTile = styled.div`
   padding: 2.5%;
   border: .5% solid orange;
   border-radius: 10px;
-<<<<<<< HEAD
 `;
-=======
-`
->>>>>>> 3bfd2c6552f9eb035fc81c974a342c055414c7b1
+
 
 export const CourseTileTitle = styled(LinkR)`
   color: orange;
@@ -36,4 +33,45 @@ export const CourseTileTitle = styled(LinkR)`
   text-decoration: none;
   cursor: pointer;
   display: flex;
+  margin-bottom: 7px;
 `;
+
+export const TodaysClassesList = styled.div`
+  background-color: black;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+`
+
+export const ClassListItem = styled.div`
+  background-color: rgb(20, 20, 20);
+  border: .1px solid rgb(20, 20, 20);
+  border-radius: 10px;
+  margin: 5px;
+  padding: 3px 10px;
+`
+
+export const ClassName = styled.h4`
+  color: orange;
+`
+
+export const ClassInfo = styled.h6`
+  color: orange;
+  word-spacing: 30px;
+`
+
+
+export const CalendarButtonCont = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-end;
+`
+
+export const CalendarButton = styled.div`
+  background-color: rgb(20, 20, 20);
+  border: 2px solid orange;
+  border-radius: 10px;
+  color: orange;
+  padding: 7px;
+  margin-top: 15px;
+`
