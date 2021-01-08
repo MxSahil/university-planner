5e81a6f4e75b96ac89523a68747639fdc83a1d60
Ansh Sahny <anshsahny@gmail.com>
2021-01-07 23:00:31 -0500

    Added new files

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.post('/user', UserCtrl.createUser);
 router.put('/user/:id', UserCtrl.updateUser);
-router.delete('/movie/:id', UserCtrl.deleteUser);
-router.get('/movie/:id', UserCtrl.getUserById);
+router.delete('/user/:id', UserCtrl.deleteUser);
+router.get('/user/:id', UserCtrl.getUserById);
 router.get('/users', UserCtrl.getUsers);
 
 module.exports = router;
